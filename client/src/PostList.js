902bf5c141b0207bd08c8f997647ad703e2e353d
Janith <janithr@xeptagon.com>
2021-12-11 18:45:47 +0530

    update

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,7 @@ export default () => {
   const [posts,setPosts] = useState({});
 
   const fetchPosts = async () =>{
-      const res = await axios.get('http://localhost:4000/posts')
+      const res = await axios.get('http://localhost:4002/posts')
       setPosts(res.data);
   }
 
@@ -21,7 +21,7 @@ export default () => {
      >
          <div className="card-body">
                <h3>{post.title}</h3>
-               <CommentLIst postId={post.id}   />
+               <CommentLIst comments={post.comments}   />
                <CommentCreate postId={post.id}  />
          </div>
      </div>;
